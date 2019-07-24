89c1ddd95e197c036414a2ad7404e3bcf0c3700b
titanmet <titanmet@mail.ru>
2019-07-24 12:04:10 +0300

    h1 red

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
   <title><?php print $title; ?></title>
   <meta content="text/html; charset=UTF-8" http-equiv="content-type">
   <link rel="stylesheet" href="style.css" >
-  <?php ShowStyle(); ?>
+  <?php //ShowStyle(); ?>
   
 </head>
 <body>
